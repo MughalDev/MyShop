24cfe7e11849e3964f302f9b0fa53e5a8723be35
mac <mac@macs-MacBook-Pro.local>
2025-08-27 14:28:05 +0500

    eslint issue fixed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,56 +1,49 @@
-// import tseslint from '@typescript-eslint/eslint-plugin';
-// import tsParser from '@typescript-eslint/parser';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 
-// export default [
-//   {
-//     ignores: ['node_modules/**', 'android/**', 'ios/**', 'build/**'],
-//   },
-//   {
-//     files: ['**/*.ts', '**/*.tsx'],
-//     languageOptions: {
-//       parser: tsParser,
-//       parserOptions: {
-//         ecmaVersion: 'latest',
-//         sourceType: 'module',
-//         project: './tsconfig.json',
-//       },
-//     },
-//     plugins: {
-//       '@typescript-eslint': tseslint,
-//     },
-//     rules: {
-//       '@typescript-eslint/no-unused-vars': 'warn',
-//       '@typescript-eslint/explicit-module-boundary-types': 'off',
-//     },
-//   },
-//   {
-//     files: ['**/*.js', '**/*.jsx'],
-//     languageOptions: {
-//       ecmaVersion: 'latest',
-//       sourceType: 'module',
-//     },
-//   },
-//   {
-//     files: [
-//       '**/*.config.js',
-//       '.prettierrc.js',
-//       'metro.config.js',
-//       'jest.config.js',
-//     ],
-//     languageOptions: {
-//       sourceType: 'commonjs',
-//       globals: {
-//         module: 'writable',
-//         require: 'readonly',
-//         __dirname: 'readonly',
-//       },
-//     },
-//   },
-// ];
-
-const { getDefaultConfig } = require('metro-config');
-
-module.exports = (async () => {
-  const defaultConfig = await getDefaultConfig();
-  return defaultConfig;
-})();
+export default [
+  {
+    ignores: ['node_modules/**', 'android/**', 'ios/**', 'build/**'],
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+    },
+  },
+  {
+    files: ['**/*.js', '**/*.jsx'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+  },
+  {
+    files: [
+      '**/*.config.js',
+      '.prettierrc.js',
+      'metro.config.js',
+      'jest.config.js',
+    ],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: {
+        module: 'writable',
+        require: 'readonly',
+        __dirname: 'readonly',
+      },
+    },
+  },
+];
