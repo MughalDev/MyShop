81414d8fe9f76452e6ac48d3fd08cf787aa5ffac
mac <mac@macs-MacBook-Pro.local>
2025-08-27 16:38:53 +0500

    web functionality added

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: [
+    'module:@react-native/babel-preset', // mobile
+    '@babel/preset-env',                 // web
+    '@babel/preset-react'                // web
+  ],
+  plugins: [
+    'react-native-web' // 👈 maps react-native imports to react-native-web
+  ]
 };
